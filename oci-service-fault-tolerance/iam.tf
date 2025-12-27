a1df3a43f62c9b83645686731f531590f6e21307
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2025-12-27 15:56:02 +0000

    adding: post-reboot configuration of floating ip

--- oci-service-fault-tolerance/iam.tf
+++ oci-service-fault-tolerance/iam.tf
@@ -9,7 +9,6 @@ resource "random_id" "name_suffix" {
 # Deterministic but unique names
 ########################################
 locals {
-  # Clean + limit base name
   base_name = substr(
     lower(replace(var.service_hostname, "/[^a-zA-Z0-9-]/", "-")),
     0,
@@ -21,22 +20,18 @@ locals {
 }
 
 ########################################
-# Dynamic Group (TENANCY level!)
+# Dynamic Group (TENANCY scope)
 ########################################
 resource "oci_identity_dynamic_group" "ha_nodes" {
   compartment_id = var.tenancy_ocid
   name           = local.dg_name
   description    = "HA nodes dynamic group for floating private IP failover"
 
-  # STRICT scoping: only instances created by this stack
-  matching_rule = "ANY {
-    instance.id = '${oci_core_instance.node1.id}',
-    instance.id = '${oci_core_instance.node2.id}'
-  }"
+  matching_rule = "ANY {instance.id = '${oci_core_instance.node1.id}', instance.id = '${oci_core_instance.node2.id}'}"
 }
 
 ########################################
-# IAM Policy (COMPARTMENT level)
+# IAM Policy (COMPARTMENT scope)
 ########################################
 resource "oci_identity_policy" "ha_nodes_policy" {
   compartment_id = var.compartment_ocid
@@ -44,13 +39,7 @@ resource "oci_identity_policy" "ha_nodes_policy" {
   description    = "Allow HA nodes to manage networking for floating IP failover"
 
   statements = [
-    # Required for:
-    # - subnet get
-    # - private-ip list / delete
-    # - vnic assign-private-ip
     "Allow dynamic-group ${oci_identity_dynamic_group.ha_nodes.name} to manage virtual-network-family in compartment id ${var.compartment_ocid}",
-
-    # Optional but useful (metadata, introspection, safety)
     "Allow dynamic-group ${oci_identity_dynamic_group.ha_nodes.name} to read instance-family in compartment id ${var.compartment_ocid}",
   ]
 }
