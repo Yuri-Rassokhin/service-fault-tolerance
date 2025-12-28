c7f0e77a4ea2e089b2d1340fbeefcb66938f79d3
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2025-12-28 12:01:12 +0000

    debug: testing DRBD self-configuration

--- generic/drbd/drbd.sh
+++ generic/drbd/drbd.sh
@@ -4,9 +4,10 @@ exec >> /var/log/ha-bootstrap.log 2>&1
 
 source /etc/ha/stack.env
 
-# remove FS label from the volume, if any
-echo "Wiping out FS label from ${BLOCK_DEVICE}"
-sudo wipefs -a "${BLOCK_DEVICE}"
+# clean up any previous DRBD, if any
+umount ${DRBD_DEVICE}
+drbdadm down "${DRBD_RESOURCE}"
+wipefs -fa "${BLOCK_DEVICE}"
 
 # Get local IP from OCI metadata
 LOCAL_IP=$(curl -s -H "Authorization: Bearer Oracle" http://169.254.169.254/opc/v2/vnics | jq -r '.[0].privateIp')
@@ -18,7 +19,8 @@ PEER_IP=$(getent hosts "${PEER_NODE_NAME}" | awk '{print $1}' || true)
 
 echo "Setting DRBD device configuration file"
 # define configuration of DRBD device
-sudo tee /etc/drbd.d/${DRBD_RESOURCE}.res <<EOF
+mkdir -p /etc/drbd.d
+tee /etc/drbd.d/${DRBD_RESOURCE}.res <<EOF
 resource ${DRBD_RESOURCE} {
     protocol C;
 
@@ -40,16 +42,14 @@ EOF
 
 echo "Spinning up DRBD device"
 # spin up DRBD device
-sudo drbdadm create-md ${DRBD_RESOURCE} || true
-sudo drbdadm up ${DRBD_RESOURCE} || true
-sudo drbdadm status ${DRBD_RESOURCE} || true
+drbdadm create-md --force ${DRBD_RESOURCE}
+drbdadm up ${DRBD_RESOURCE} || true
+drbdadm status ${DRBD_RESOURCE} || true
 
 echo "Preparing mount point for DRBD device"
 # prepare mount point for DRBD device
-sudo mkdir -p "${MOUNT_POINT}"
-sudo chown -R "$USER:$USER" "${MOUNT_POINT}"
-
-
+mkdir -p "${MOUNT_POINT}"
+chown -R "$USER:$USER" "${MOUNT_POINT}"
 
 # Define who's Primary and who's Secondary
 # We use simple lexicographical ordering to avoid ambiguity / race conditions
@@ -68,7 +68,7 @@ if [[ "$ROLE" == "primary" ]]; then
 
   if ! blkid ${DRBD_DEVICE} >/dev/null 2>&1; then
     echo "Creating filesystem"
-    mkfs.${FS_TYPE} ${DRBD_DEVICE}
+    mkfs -t ${FS_TYPE} ${DRBD_DEVICE}
   fi
 
   if ! mountpoint -q "${MOUNT_POINT}"; then
@@ -77,4 +77,3 @@ if [[ "$ROLE" == "primary" ]]; then
 else
   echo "Current node is secondary, skipping filesystem setup"
 fi
-
