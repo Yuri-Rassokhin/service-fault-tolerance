778adc2a8a03641253b196be19208abfa1ff3a27
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2025-12-29 07:51:28 +0000

    debugging: unambiguous storage attachment

--- generic/storage.sh
+++ generic/storage.sh
@@ -2,6 +2,16 @@
 set -euo pipefail
 exec >> /var/log/ha-bootstrap.log 2>&1
 
-echo "Configuring HA networking"
+echo "Configuring HA storage"
+exit
+
+# TODO: 
+# 1. Use OCI CLI to get iSCSI attachment command and attach teh volume
+# 2. Provide stable device path to the HA status file
+
+#oci compute volume-attachment list --instance-id <instance_OCID>
+
 BLOCK_DEVICE=$(lsblk -ndo NAME,TYPE | grep -v sda | awk '$2=="disk"{print $1}')
-echo "BLOCK_DEVICE=/dev/${BLOCK_DEVICE}" >> /etc/ha/stack.env
+#echo "BLOCK_DEVICE=/dev/${BLOCK_DEVICE}" >> /etc/ha/stack.env
+
+echo "HA storage configured"
