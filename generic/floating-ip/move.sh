360fcb4c2770ee5d2ff222bddcf396c91c1e8f30
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2026-01-01 21:10:50 +0000

    added: integration of floating IP to Pacemaker

--- generic/floating-ip/move.sh
+++ generic/floating-ip/move.sh
@@ -14,7 +14,7 @@ fi
 source "${STATE_FILE}"
 
 # Reassign to the current VNIC
-oci network private-ip update --private-ip-id "$SERVICE_IP_OCID" --vnic-id "$VNIC_OCID"
+oci network private-ip update --private-ip-id "$SERVICE_IP_OCID"
 # Make it visible in the OS
 ip addr add ${SERVICE_IP}/24 dev ${IFACE} 2>/dev/null || true
 
