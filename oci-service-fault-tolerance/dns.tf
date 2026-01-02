27236e1a263c5a81d961835dd3ad69ae48d267e0
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2026-01-02 19:26:01 +0000

    debug: DNS terraform

---
+++ oci-service-fault-tolerance/dns.tf
@@ -0,0 +1,101 @@
+############################
+# DNS Resolver (existing)
+############################
+
+data "oci_dns_resolvers" "vcn_resolvers" {
+  compartment_id = var.compartment_ocid
+  scope          = "PRIVATE"
+}
+
+locals {
+  vcn_resolver_id = one([
+    for r in data.oci_dns_resolvers.vcn_resolvers.resolvers :
+    r.id if r.attached_vcn_id == var.vcn_ocid
+  ])
+}
+
+############################
+# Private DNS View
+############################
+
+resource "oci_dns_view" "resilient" {
+  compartment_id = var.compartment_ocid
+  scope          = "PRIVATE"
+  display_name   = "resilient-view"
+}
+
+############################
+# Attach View to Resolver
+############################
+
+resource "oci_dns_resolver" "attach_view" {
+  resolver_id = local.vcn_resolver_id
+
+  attached_views {
+    view_id = oci_dns_view.resilient.id
+  }
+}
+
+############################
+# Private DNS Zone
+############################
+
+resource "oci_dns_zone" "resilient" {
+  compartment_id = var.compartment_ocid
+  name           = "resilient.oci."
+  zone_type      = "PRIMARY"
+  scope          = "PRIVATE"
+  view_id        = oci_dns_view.resilient.id
+}
+
+data "oci_dns_zones" "private_zones" {
+  compartment_id = var.compartment_ocid
+  scope          = "PRIVATE"
+}
+
+locals {
+  resilient_zone_name = "resilient.oci."
+
+  existing_resilient_zone = try(
+    one([
+      for z in data.oci_dns_zones.private_zones.zones :
+      z if z.name == local.resilient_zone_name
+    ]),
+    null
+  )
+}
+
+resource "oci_dns_zone" "resilient" {
+  count          = local.existing_resilient_zone == null ? 1 : 0
+
+  compartment_id = var.compartment_ocid
+  name           = local.resilient_zone_name
+  zone_type      = "PRIMARY"
+  scope          = "PRIVATE"
+
+  freeform_tags = {
+    managed-by = "terraform"
+    stack      = "ha-drbd"
+  }
+}
+
+locals {
+  resilient_zone_ocid = local.existing_resilient_zone != null
+    ? local.existing_resilient_zone.id
+    : oci_dns_zone.resilient[0].id
+}
+
+
+
+############################
+# Outputs to HA stack
+############################
+
+output "dns_zone_name" {
+  value = local.resilient_zone_name
+}
+
+output "dns_zone_ocid" {
+  value = local.resilient_zone_ocid
+}
+
