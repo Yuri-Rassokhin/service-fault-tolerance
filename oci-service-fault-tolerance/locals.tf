6105162b6302b0b940ba17ed1a481e77d234a4e8
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2026-01-11 20:30:00 +0000

    debugging: relaying cross-AD mode in variables

--- oci-service-fault-tolerance/locals.tf
+++ oci-service-fault-tolerance/locals.tf
@@ -3,11 +3,11 @@ locals {
   ad_primary   = local.ad_names[0]
   ad_secondary = var.cross_ad_fault_tolerance ? local.ad_names[1] : local.ad_names[0]
 
+  # Location
+  region = var.cross_ad_fault_tolerance ? var.region_multi_ad : var.region_single_ad
+
   # Network consistency
   vcn_region_ok = data.oci_core_vcn.selected.region == var.region
   subnet_in_vcn = data.oci_core_subnet.selected.vcn_id == var.vcn_ocid
-
-  # Location
-  region = var.cross_ad_fault_tolerance ? var.region_multi_ad : var.region_single_ad
 }
 
