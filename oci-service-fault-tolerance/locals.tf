748f5bacb36e271977a43e932c31f7484f9341a5
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2026-01-12 11:34:41 +0000

    debugging: reworking cross-AD variables

--- oci-service-fault-tolerance/locals.tf
+++ oci-service-fault-tolerance/locals.tf
@@ -1,7 +1,7 @@
 locals {
   region = var.cross_ad_fault_tolerance ? var.region_multi_ad : var.region_single_ad
   ad_names = data.oci_identity_availability_domains.ads.availability_domains[*].name
-  ad_primary = element(local.ad_names, 0)
-  ad_secondary = element(local.ad_names, -1)
+  ad_primary = local.ad_names[0]
+  ad_secondary = local.ad_names[length(local.ad_names) - 1]
 }
 
